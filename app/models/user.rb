bd789b88a21d9bf29ebdbdd8626784376de63e96
Julius <albatross@cannon.lt>
2026-08-30 23:35:07 +0300

    Generate auth token for user by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :conversations, dependent: :destroy
 
+  before_create { self.authentication_token = SecureRandom.hex(20) if authentication_token.blank? }
+
   def self.find_for_authentication(conditions)
     if conditions[:authentication_token].present?
       find_by(authentication_token: conditions[:authentication_token])
