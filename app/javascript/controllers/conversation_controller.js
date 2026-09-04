36411a7d3922e31971c20bece3418bb8c4f3b76a
Julius <razorbill@cannon.lt>
2026-09-04 20:15:31 +0300

    Fix auto scroll on just opened old conversation

--- app/javascript/controllers/conversation_controller.js
+++ app/javascript/controllers/conversation_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   connect() {
     this.mutationObserver = new MutationObserver((mutations) => this.handleMutation(mutations))
     this.mutationObserver.observe(this.element, { childList: true, subtree: true })
+    requestAnimationFrame(() => this.scrollToLatest())
   }
 
   disconnect() {
